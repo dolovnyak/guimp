d0d9cd8275f4af8db0d09bd607f34d73393acac6
Slaughterfish Becker <sbecker@at-f5.21-school.ru>
2019-07-15 17:45:12 +0300

    layers

--- src/layers/add_layer.c
+++ src/layers/add_layer.c
@@ -6,11 +6,51 @@
 /*   By: sbecker <sbecker@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/15 16:58:19 by sbecker           #+#    #+#             */
-/*   Updated: 2019/07/15 17:13:06 by sbecker          ###   ########.fr       */
+/*   Updated: 2019/07/15 17:33:24 by sbecker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "guimp.h"
+
+t_ui_el	*create_tmp_el(t_guimp *g, t_ui_el *layer_menu)
+{
+	t_ui_el		*tmp_el;
+
+	tmp_el = ui_el_init();
+	ui_el_setup_default(tmp_el);
+	ui_el_setup_default_scroll_menu_elem(tmp_el);
+	ui_el_add_child(layer_menu, tmp_el);
+	tmp_el->id = gm_generate_surf_id(ID_GENERATOR_ADD);
+	ui_el_set_new_pos(tmp_el, 0, 0,
+		(t_fvec2){0.0, ((t_ui_el *)layer_menu->children->content)->rrect.y
+		+ 0.25f * (float)gm_generator_get_surf_count()});
+	ui_el_set_size(tmp_el, 0, (t_fvec2){1, 0.25});
+	tmp_el->sdl_renderer = g->main_win->sdl_renderer;
+	ui_el_add_color_texture(tmp_el, (t_vec2){1704, 800}, tmp_el->id % 2 ? 0x88AA88 : 0x669966, "default");
+	ui_el_add_color_texture(tmp_el, (t_vec2){1704, 800}, 0xFF5050, "onActive");
+	ui_el_add_color_texture(tmp_el, (t_vec2){1704, 800}, 0x5050FF, "onFocus");
+	ui_event_add_listener(tmp_el->events->on_pointer_left_button_pressed, OnPtrLBD);
+	ui_event_add_listener(tmp_el->events->on_pointer_enter, OnPtrEnter);
+	ui_event_add_listener(tmp_el->events->on_pointer_left_button_pressed, PressedLBD);
+	ui_event_add_listener(tmp_el->events->on_pointer_exit, OnPtrExit);
+	return (tmp_el);
+}
+
+t_ui_el	*create_el(t_guimp *g, t_ui_el *tmp_el)
+{
+	t_ui_el	*el;
+
+	el = ui_el_init();
+	ui_el_setup_default(el);
+	ui_el_add_child(tmp_el, el);
+	ui_el_set_pos(el, 0, (t_fvec2){0.04, 0.05});
+	ui_el_set_size(el, 0, (t_fvec2){0.92, 0.782});
+	el->id = tmp_el->id * 1000;
+	el->params |= EL_IGNOR_RAYCAST | EL_IS_DEPENDENT;
+	el->sdl_renderer = g->main_win->sdl_renderer;
+	ui_el_add_empty_texture(el, GM_IMAGE_SIZE_X, GM_IMAGE_SIZE_Y, "default");
+	return (el);
+}
 
 int add_layer(t_ui_main *m, void *el_v)
 {
@@ -22,44 +62,11 @@ int add_layer(t_ui_main *m, void *el_v)
 
 	el = (t_ui_el *)el_v;
 	g = (t_guimp *)m->data;
-	if (gm_generator_get_surf_count() > 20)
+	if (gm_generator_get_surf_count() > 30)
 		return (1);
 	layer_menu = ui_win_find_el_by_id(g->main_win, GM_LAYER_ID_MENU);
-	if (!(tmp_el = ui_el_init()))
-	{
-		printf("layer malloc error in scrollable menu in layer_win\n");
-		return (1);
-	}
-	ui_el_setup_default(tmp_el);
-	ui_el_setup_default_scroll_menu_elem(tmp_el);
-	ui_el_add_child(layer_menu, tmp_el);
-	tmp_el->id = gm_generate_surf_id(ID_GENERATOR_ADD);
-	ui_el_set_new_pos(tmp_el, 0, 0,
-		(t_fvec2){0.0,
-			((t_ui_el *)layer_menu->children->content)->rrect.y + 0.25f * (float)gm_generator_get_surf_count()});// * layer_menu->crect.x / g->main_win->size.y * (float)gm_generator_get_surf_count()});
-	ui_el_set_size(tmp_el, 0, (t_fvec2){1, 0.25});
-	tmp_el->sdl_renderer = g->main_win->sdl_renderer;
-	ui_el_add_color_texture(tmp_el, (t_vec2){1704, 800}, tmp_el->id % 2 ? 0x88AA88 : 0x669966, "default");
-	ui_el_add_color_texture(tmp_el, (t_vec2){1704, 800}, 0xFF5050, "onActive");
-	ui_el_add_color_texture(tmp_el, (t_vec2){1704, 800}, 0x5050FF, "onFocus");
-	ui_event_add_listener(tmp_el->events->on_pointer_left_button_pressed, OnPtrLBD);
-	ui_event_add_listener(tmp_el->events->on_pointer_enter, OnPtrEnter);
-	ui_event_add_listener(tmp_el->events->on_pointer_left_button_pressed, PressedLBD);
-	ui_event_add_listener(tmp_el->events->on_pointer_exit, OnPtrExit);
-
-	if (!(el = ui_el_init()))
-	{
-		printf("layer texture malloc error in scrollable menu in layer_win\n");
-		return (1);
-	}
-	ui_el_setup_default(el);
-	ui_el_add_child(tmp_el, el);
-	ui_el_set_pos(el, 0, (t_fvec2){0.04, 0.05});
-	ui_el_set_size(el, 0, (t_fvec2){0.92, 0.782});
-	el->id = tmp_el->id * 1000;
-	el->params |= EL_IGNOR_RAYCAST | EL_IS_DEPENDENT;
-	el->sdl_renderer = g->main_win->sdl_renderer;
-	ui_el_add_empty_texture(el, GM_IMAGE_SIZE_X, GM_IMAGE_SIZE_Y, "default");
+	tmp_el = create_tmp_el(g, layer_menu);
+	el = create_el(g, tmp_el);
 
 	t_list	*tmp;
 	tmp = ft_lstnew(NULL, 0);
--- src/layers/clear_layers.c
+++ src/layers/clear_layers.c
@@ -6,13 +6,13 @@
 /*   By: sbecker <sbecker@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/15 16:53:12 by sbecker           #+#    #+#             */
-/*   Updated: 2019/07/15 17:02:34 by sbecker          ###   ########.fr       */
+/*   Updated: 2019/07/15 17:19:22 by sbecker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "guimp.h"
 
-int clear_layer(t_ui_main *main, void *el_v)
+int	clear_layer(t_ui_main *main, void *el_v)
 {
 	t_texture	*t;
 	t_guimp		*g;
@@ -21,13 +21,14 @@ int clear_layer(t_ui_main *main, void *el_v)
 	t = (t_texture *)((t_ui_el *)el_v)->data;
 	ui_sdl_set_render_target(g->main_win->sdl_renderer, t);
 	ui_sdl_set_render_draw_blend_mode(g->main_win->sdl_renderer, NONE);
-	ui_sdl_set_render_draw_color(g->main_win->sdl_renderer, &(t_color){255, 255, 255, 0});
+	ui_sdl_set_render_draw_color(g->main_win->sdl_renderer,
+			&(t_color){255, 255, 255, 0});
 	ui_sdl_render_fill_rect(g->main_win->sdl_renderer, NULL);
 	ui_sdl_set_render_target(g->main_win->sdl_renderer, NULL);
 	return (1);
 }
 
-int clear_all_layers(t_ui_main *main, void *el_v)
+int	clear_all_layers(t_ui_main *main, void *el_v)
 {
 	t_texture	*t;
 	t_list		*l;
@@ -41,11 +42,11 @@ int clear_all_layers(t_ui_main *main, void *el_v)
 		t = (t_texture *)l->content;
 		ui_sdl_set_render_target(g->main_win->sdl_renderer, t);
 		ui_sdl_set_render_draw_blend_mode(g->main_win->sdl_renderer, NONE);
-		ui_sdl_set_render_draw_color(g->main_win->sdl_renderer, &(t_color){255, 255, 255, 0});
+		ui_sdl_set_render_draw_color(g->main_win->sdl_renderer,
+				&(t_color){255, 255, 255, 0});
 		ui_sdl_render_fill_rect(g->main_win->sdl_renderer, NULL);
 		l = l->next;
 	}
 	ui_sdl_set_render_target(g->main_win->sdl_renderer, NULL);
 	return (1);
 }
-
