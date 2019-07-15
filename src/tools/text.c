c38022410a27851574acd668518946c8987128d7
Etienne Draugr death overlord <edraugr-@oa-l1.21-school.ru>
2019-07-15 03:37:52 +0300

    text finaly ready

--- src/tools/text.c
+++ src/tools/text.c
@@ -6,7 +6,7 @@
 /*   By: edraugr- <edraugr-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/12 17:49:44 by edraugr-          #+#    #+#             */
-/*   Updated: 2019/07/15 01:10:55 by edraugr-         ###   ########.fr       */
+/*   Updated: 2019/07/15 03:32:58 by edraugr-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,18 @@ int	choose_font(t_ui_main *main, void *el_v)
 	t_guimp	*g;
 	t_ui_el	*el;
 	t_ui_el	*el_target;
+	char	*text;
 
 	g = (t_guimp *)(main->data);
 	el = (t_ui_el *)el_v;
+	text = NULL;
 	el_target = ui_win_find_el_by_id(g->tool_win, 121210);
 	el = (t_ui_el *)el->children->content;
 	el_target->text_area->font = el->text_area->font;
-	ui_el_update_text(el_target, el_target->text_area->text);
+	text = ft_strdup(el_target->text_area->text);
+	ui_el_update_text(el_target, text);
+	if (text)
+		free(text);
 	return (1);
 }
 
@@ -42,12 +47,17 @@ int	update_text_color(t_ui_main *main, void *el_v)
 {
 	t_guimp	*g;
 	t_ui_el	*el;
+	char	*text;
 
 	g = (t_guimp *)(main->data);
 	el = (t_ui_el *)el_v;
+	text = NULL;
 	el->text_area->text_color = (t_color){g->draw_tool.r,
 		g->draw_tool.g, g->draw_tool.b, g->draw_tool.a};
-	ui_el_update_text(el, el->text_area->text);
+	text = ft_strdup(el->text_area->text);
+	ui_el_update_text(el, text);
+	if (text)
+		free(text);
 	return (1);
 }
 
